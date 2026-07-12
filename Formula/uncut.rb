d9d594a728b1b693f002f1d614ab003f34d2b205
Uncut Maintainers <noreply@uncut.invalid>
2026-07-12 13:49:12 -0500

    Update uncut to 0.1.4

--- Formula/uncut.rb
+++ Formula/uncut.rb
@@ -1,14 +1,14 @@
 class Uncut < Formula
   desc "Uncut CLI"
   homepage "https://github.com/simmmis/uncut"
-  version "0.1.3"
+  version "0.1.4"
 
   if Hardware::CPU.arm?
-    url "https://github.com/simmmis/uncut/releases/download/v0.1.3/uncut-darwin-arm64.tar.gz"
-    sha256 "a9cf17de8fa15299d863c9808de8a14a6c6d1e044b272fccddfcd60fabdbed23"
+    url "https://github.com/simmmis/uncut/releases/download/v0.1.4/uncut-darwin-arm64.tar.gz"
+    sha256 "610e8a99049d894c7f601d12fd1ccf35b891e188cd29d6bb831c6c1605330319"
   else
-    url "https://github.com/simmmis/uncut/releases/download/v0.1.3/uncut-darwin-amd64.tar.gz"
-    sha256 "b2f5abb942413d0c820ad72bcc58dde0fe137d96e1df25a52cd8c6cc5dbb0276"
+    url "https://github.com/simmmis/uncut/releases/download/v0.1.4/uncut-darwin-amd64.tar.gz"
+    sha256 "de6863375e0e8721a36639e3f7693254cea1a209dfc583603ad2eed0ce0683b9"
   end
 
   def install
